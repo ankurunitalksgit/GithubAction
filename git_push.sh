48db939469287040488ec960c66a6d13b548a453
ankurunitalksgit <ankur@unitalks.co>
2020-10-07 15:58:46 +0530

    added the build request

---
+++ git_push.sh
@@ -0,0 +1,13 @@
+echo "Enter your message"
+
+read messag:e
+git add .
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin master
+fi
